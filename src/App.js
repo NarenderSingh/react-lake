a2987f4744c6fd58c861b10c9df1eb5a5fcadda7
NarenderSingh <naren.delhi13@gmail.com>
2019-12-22 17:01:33 +0530

    added App1 component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+// Functional Component
 const App = ({ lakes }) => (
   <ul>
-      {lakes.map((lake, i) => 
-          <li key={i}>{lake.name}</li>
-      )}
+    {lakes.map((lake, i) => (
+      <li key={i}>{lake.name}</li>
+    ))}
   </ul>
-)
+);
+
+// State Component
+class App1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Welcome to React Lake App</h4>
+      </div>
+    );
+  }
+}
 
 export default App;
-
