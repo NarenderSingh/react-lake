cd888b7e7eb9d931010972e3c23eef63322a4c64
NarenderSingh <naren.delhi13@gmail.com>
2019-12-22 16:53:32 +0530

    changes for Lake App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const lakeList = [
+  { id: 1, name: "Echo Lake", trailhead: "Echo Lake" },
+  { id: 2, name: "Maud Lake", trailhead: "Wright's Lake" },
+  { id: 3, name: "Cascade Lake", trailhead: "Bayview" }
+];
+
+ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
